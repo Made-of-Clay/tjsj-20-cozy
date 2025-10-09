17c840ff6d158c4567207ff1dbb525e48c24b993
Adam Leis <adam.m.leis@gmail.com>
2025-10-09 07:01:36 -0400

    add vite config

---
+++ vite.config.ts
@@ -0,0 +1,13 @@
+/// <reference types="vite/client" />
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+    build: {
+        target: 'esnext',
+    },
+    esbuild: {
+        supported: {
+            'top-level-await': true,
+        },
+    },
+});
