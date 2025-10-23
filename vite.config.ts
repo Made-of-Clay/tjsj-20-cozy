46a27ecc784e74545be44adca15d0b7e338fc983
Adam Leis <adam.m.leis@gmail.com>
2025-10-22 21:56:45 -0400

    add better vendor chunking

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,16 @@ import { defineConfig } from 'vite';
 export default defineConfig({
     build: {
         target: 'esnext',
+        rollupOptions: {
+            output: {
+                manualChunks(id) {
+                    if (id.includes('node_modules')) {
+                        return 'vendor';
+                    }
+                    return undefined;
+                },
+            },
+        }
     },
     esbuild: {
         supported: {
